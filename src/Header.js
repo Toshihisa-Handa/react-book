feaab36664e6755c2d4d7c430c27399ff9c57ebf
Toshihisa-Handa <64568855+Toshihisa-Handa@users.noreply.github.com>
2020-05-29 13:02:21 +0900

    初めてのリアクト　記述したのはsrcフォルダのみ
    1回目のコミット

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () =>{
+  return(
+    <header>
+      <i className="fas fa-book fa-2x"></i>
+      <h1>Book cards</h1>
+    </header>
+  )
+
+}
+
+export default Header;
